a8394bb20d65025491f5b88f318f8d8f6551d6c0
Gobinda Sahoo <gobinda.sahoo@pixelcompute.com>
2025-04-08 02:39:19 +0000

    Added organization model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_25_133940) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_08_023349) do
+  create_table "organizations", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
